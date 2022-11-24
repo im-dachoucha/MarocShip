a6792a348caa720551b4a1d5b0ad63722bc59222
im-dachoucha <im.dachoucha@gmail.com>
2022-11-24 16:24:30 +0100

    add chauffeur bean

---
+++ MaorocShip/src/main/java/com/simplon/maorocship/Beans/Chauffeur.java
@@ -0,0 +1,40 @@
+package com.simplon.maorocship.Beans;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class Chauffeur {
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
