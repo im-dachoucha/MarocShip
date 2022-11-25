6bbd886468b19df45d2bc57d2212f1ad3e7894c8
im-dachoucha <im.dachoucha@gmail.com>
2022-11-25 16:48:04 +0100

    login and redirect to different page

--- MaorocShip/src/main/java/com/simplon/marocship/Main.java
+++ MaorocShip/src/main/java/com/simplon/marocship/Main.java
@@ -1,16 +1,25 @@
 package com.simplon.marocship;
 
+import com.simplon.marocship.Entities.RespLivraisonEntity;
 import com.simplon.marocship.Entities.VehiculeEntity;
+import com.simplon.marocship.dao.RespLivraisonDao;
 import com.simplon.marocship.dao.VehiculeDao;
 import com.simplon.marocship.services.JpaService;
+import com.simplon.marocship.services.Security;
 
 public class Main {
 
         public static void main(String[] args) {
-            VehiculeEntity vehiculeEntity = new VehiculeEntity();
-            vehiculeEntity.setType("grand-camion");
-            VehiculeDao vehiculeDao = new VehiculeDao();
-            vehiculeDao.create(vehiculeEntity);
+            RespLivraisonDao respLivraisonDao = new RespLivraisonDao();
+            RespLivraisonEntity respLivraison = new RespLivraisonEntity();
+            respLivraison.setEmail("resp1@gmail.com");
+            respLivraison.setPassword("test");
+            respLivraison.setName("response one");
+            respLivraisonDao.hashPasswordThenCreat(respLivraison);
+//            VehiculeEntity vehiculeEntity = new VehiculeEntity();
+//            vehiculeEntity.setType("grand-camion");
+//            VehiculeDao vehiculeDao = new VehiculeDao();
+//            vehiculeDao.create(vehiculeEntity);
 //            JpaService jpaService = new Jpa:
 
         }
--- MaorocShip/src/main/java/com/simplon/marocship/dao/RespLivraisonDao.java
+++ MaorocShip/src/main/java/com/simplon/marocship/dao/RespLivraisonDao.java
@@ -1,6 +1,7 @@
 package com.simplon.marocship.dao;
 
 import com.simplon.marocship.Entities.RespLivraisonEntity;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class RespLivraisonDao  extends AbstractHibernateDao<RespLivraisonEntity> {
     private Class<RespLivraisonEntity> clazz;
@@ -10,10 +11,23 @@ public class RespLivraisonDao  extends AbstractHibernateDao<RespLivraisonEntity>
     }
 
     public RespLivraisonEntity findByEmail(String email) {
+        try{
         return jpaService.runInTransaction(entityManager -> {
-            return (RespLivraisonEntity) entityManager.createQuery("from " + clazz.getName() + " where email = :email")
+            return (RespLivraisonEntity) entityManager.createQuery("select c from RespLivraisonEntity c where c.email like :email", RespLivraisonEntity.class)
                     .setParameter("email", email)
                     .getSingleResult();
+        });
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean hashPasswordThenCreat(RespLivraisonEntity respLivraisonEntity) {
+        respLivraisonEntity.setPassword(BCrypt.hashpw(respLivraisonEntity.getPassword(), BCrypt.gensalt()));
+        return jpaService.runInTransaction(entityManager -> {
+            entityManager.persist(respLivraisonEntity);
+            return true;
         });
     }
 
